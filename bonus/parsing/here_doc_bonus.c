15a446032271e3c514be7d2ec3ec24fa852a5680
Zwhea <theawang@yahoo.com>
2023-01-26 13:46:11 +0100

    here_doc fixed

--- bonus/parsing/here_doc_bonus.c
+++ bonus/parsing/here_doc_bonus.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   here_doc_bonus.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: twang <twang@student.42.fr>                +#+  +:+       +#+        */
+/*   By: wangthea <wangthea@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/17 19:44:48 by wangthea          #+#    #+#             */
-/*   Updated: 2023/01/23 10:59:42 by twang            ###   ########.fr       */
+/*   Updated: 2023/01/26 13:43:42 by wangthea         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,26 +17,37 @@ void	is_here_doc(char *av, t_pipex *data)
 	data->here_doc = (av && ft_strncmp("here_doc", av, 9) == 0);
 }
 
-void	get_here_doc(char *av, t_pipex *data)
+void	here_doc_to_file(char *av, int file)
 {
-	int		file;
 	char	*buffer;
+	int		av_length;
+	int		buffer_length;
 
-	file = open("here_doc", O_CREAT | O_WRONLY | O_TRUNC, 0644);
-	if (file == -1)
-		perror("open");
 	while (1)
 	{
 		ft_dprintf(1, "here_doc> ");
 		buffer = get_next_line(0);
 		if (!buffer)
 			exit(1);
-		if (!ft_strncmp(av, buffer, ft_strlen(buffer) - 1))
+		av_length = ft_strlen(av);
+		buffer_length = ft_strlen(buffer);
+		if (!ft_strncmp(av, buffer, buffer_length - 1) \
+			&& av_length == buffer_length - 1)
 			break ;
 		ft_dprintf(file, "%s", buffer);
 		free(buffer);
 	}
 	free(buffer);
+}
+
+void	get_here_doc(char *av, t_pipex *data)
+{
+	int		file;
+
+	file = open("here_doc", O_CREAT | O_WRONLY | O_TRUNC, 0644);
+	if (file == -1)
+		perror("open");
+	here_doc_to_file(av, file);
 	close(file);
 	data->infile = open("here_doc", O_RDONLY);
 	if (data->infile == -1)
